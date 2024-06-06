5efc03ad489bfaedb9b7e8052e66f722fae74f2b
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-06-06 12:47:44 +0100

    QnA model

--- Labfiles/02-qna/ask-question.sh
+++ Labfiles/02-qna/ask-question.sh
@@ -1,6 +1,6 @@
 
-prediction_url="YOUR_ENDPOINT_URL"
-key="YOUR_KEY"
+prediction_url="https://lang-1413.cognitiveservices.azure.com/"
+key=""
 
 curl -X POST $prediction_url -H "Ocp-Apim-Subscription-Key: $key" -H "Content-Type: application/json" -d "{'question': 'What is a learning Path?' }"
 
